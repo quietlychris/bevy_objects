ba9437fb908e1cdd8e55a83a8c0e0f19462e5268
chrism <cmoran@cmoran.xyz>
2022-10-05 21:55:15 -0400

    Spawn colliders iteratively as children

--- src/main.rs
+++ src/main.rs
@@ -55,13 +55,17 @@ fn use_my_assets(
     mut assets_mesh: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // do something using the asset handles from the resource
-    // println!("{:?}",assets.object);
-    //dbg!(&assets.img);
     // Add a camera so we can see the debug-render.
-
+    let cr = 4.0; // camera ratio
     commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(5.5, 0.5, 1.5).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(1.0 * cr, 0.25 * cr, 0.25 * cr).looking_at(
+            Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            Vec3::Y,
+        ),
         ..Default::default()
     });
 
@@ -117,72 +121,50 @@ fn use_my_assets(
     commands
         .spawn_bundle(cube)
         .insert(Object)
-        // .insert(Collider::cuboid(1.5, 0., 1.5))
         .insert(Friction::coefficient(0.9))
-        .insert(Restitution::coefficient(0.1))
+        .insert(Restitution::coefficient(0.))
         .insert(Collider::from_bevy_mesh(rectangle_mesh, &trimesh).unwrap())
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0., 0.0)));
 
     let obj = assets_gltf.get(&assets.object).unwrap();
     dbg!(&obj);
 
-    // let gltf_mesh = assets_gltf_mesh.get(&obj.named_meshes["Pad"]).unwrap();
-    let gltf_mesh = assets_gltf_mesh.get(&obj.meshes[0]).unwrap();
-    let mesh_handle_object = gltf_mesh.primitives[0].mesh.clone();
-    let mesh = assets_mesh.get(&mesh_handle_object).unwrap();
-
-    let collider = Collider::from_bevy_mesh(mesh, &trimesh).unwrap();
-
-    let obj_scale = 3.0;
-    let object = PbrBundle {
-        mesh: mesh_handle_object,
-        /*
-        material: materials.add(StandardMaterial {
-            base_color: Color::hex("cc0000").unwrap(),
-            // vary key PBR parameters on a grid of spheres to show the effect
-            metallic: 0.1,
-            perceptual_roughness: 0.1,
-            ..Default::default()
-        }),
-        */
-        transform: Transform {
-            translation: Vec3 {
-                x: 0.0,
-                y: 3.0,
-                z: 0.0,
-            },
-            rotation: Quat::from_euler(EulerRot::XYZ, 45.0, 45.0, 0.0),
-            scale: Vec3::new(obj_scale, obj_scale, obj_scale),
-        },
-        ..Default::default()
-    };
-
     let scene = SceneBundle {
-        //scene: assets_scenes.get(&obj.scenes[0]).unwrap(),
-        scene: obj.scenes[0].clone(),
-        //transform: Transform::from_xyz(0.0, 3.0, 0.0),
+        scene: obj.default_scene.clone().unwrap(),
         transform: Transform {
             translation: Vec3 {
                 x: 0.0,
-                y: 3.0,
+                y: 1.0,
                 z: 0.0,
             },
-            rotation: Quat::from_euler(EulerRot::XYZ, 45.0, 45.0, 0.0),
-            scale: Vec3::new(obj_scale, obj_scale, obj_scale),
+            rotation: Quat::from_euler(EulerRot::XYZ, 45.0, -45.0, 0.0),
+            ..Default::default() // scale: Vec3::new(obj_scale, obj_scale, obj_scale),
         },
         ..default()
     };
 
+    let gltf_mesh = assets_gltf_mesh.get(&obj.meshes[0]).unwrap();
+
     commands
         .spawn_bundle(scene)
-        // .spawn_bundle(object)
         .insert(Object)
         .insert(RigidBody::Dynamic)
-        .insert(collider)
-        .insert(GravityScale(0.1))
-        .insert(Friction::coefficient(0.7))
-        .insert(Restitution::coefficient(0.3))
-        .insert(ColliderMassProperties::Density(2.0));
+        .with_children(|children| {
+            for i in 0..gltf_mesh.primitives.len() {
+                let mesh_handle_object = gltf_mesh.primitives[i].mesh.clone();
+                let mesh = assets_mesh.get(&mesh_handle_object).unwrap();
+                let collider = Collider::from_bevy_mesh(mesh, &trimesh).unwrap();
+                children.spawn().insert(collider);
+            }
+        })
+        .insert(Damping {
+            linear_damping: 0.5,
+            angular_damping: 0.5,
+        })
+        .insert(GravityScale(1.0))
+        .insert(Friction::coefficient(10.0))
+        .insert(Restitution::coefficient(0.1))
+        .insert(ColliderMassProperties::Density(1000.0));
 }
 
 fn print_obj_altitude(positions: Query<&Transform, With<RigidBody>>) {
